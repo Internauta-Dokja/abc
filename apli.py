f4b530dfa3ead999eaaa5df75951a29ca83acb82
Yesenia Zuñiga <2113110050@untels.edu.pe>
2024-05-19 17:29:21 -0500

    vamos2

--- apli.py
+++ apli.py
@@ -3,7 +3,7 @@ def validar_credenciales(login, clave):
     with open("login.txt", "r") as login_file, open("clave.txt", "r") as clave_file:
         logins = login_file.readlines()
         claves = clave_file.readlines()
-        if login + "\n" in logins and clave + "\n" in claves:
+        if login  in logins and clave  in claves:
             return True
         else:
             return False
